891fa6857ec6f8682fdc589b0fcd08075da36177
skvinderr <80159890+skvinderr@users.noreply.github.com>
2021-10-15 16:48:37 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,27 +1,45 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
+import os
+
+with open("README.md", "r", encoding="utf8") as fh:
+    long_description = fh.read()
+
+with open(os.path.join("hellobomb", ".version"), "r", encoding="utf8") as fh:
+    version = fh.read().strip()
+
+with open("requirements.txt", "r", encoding="utf8") as fh:
+    requirements = fh.read().strip().splitlines()
+
+
 setup(
-  name = 'Hellobomb',         # How you named your package folder (MyLib)
-  packages = ['Hellobomb'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
-  license='afl-3.0',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'This is a SMS And CALL and MAIL bomber , by ADI',   # Give a short description about your library
-  author = 'ADITYA',                   # Type in your name
-  author_email = 'skvinderr@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/skvinderr/Hellobomb',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/skvinderr/Hellobomb/archive/refs/heads/master.zip',    # I explain this later on
-  keywords = ['Bomber', 'SMS bomber', 'Call bomber', 'Free bomber'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
-      ],
-  classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: Academic Free License v3.0',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-  ],
+    name='hellobomb',
+    packages=find_packages(),
+    include_package_data=True,
+    version=version,
+    description='A free and open-source SMS,Call & Mail bombing application',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='ADITYA',
+    author_email='skvinderr@gmail.com',
+    url='https://github.com/skvinderr/Hellobomb',
+    download_url="https://github.com/skvinderr/Hellobomb/archive/refs/heads/pypi.zip",
+        keywords=['android', 'spam', 'sms', 'mail', 'tbomb',
+                  'call', 'bomb', 'termux', 'bomber', 'spammer'],
+    classifiers=[
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Developers',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+            'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+            'Programming Language :: Python :: 3',
+            'Operating System :: OS Independent',
+            'Environment :: Console',
+    ],
+    install_requires=requirements,
+    license='GPL',
+    entry_points={
+            'console_scripts': [
+                'tbomb = tbomb.bomber:main',
+            ],
+    },
+    python_requires='>=3.5'
 )
